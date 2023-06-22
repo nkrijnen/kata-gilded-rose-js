1d301b70966032ee16108705e555cc4023ca3f82
Nico Krijnen <nico.krijnen@luminis.eu>
2023-06-22 11:58:05 +0200

    Change from jest to vite

--- src/gilded-rose.test.ts
+++ src/gilded-rose.test.ts
@@ -1,9 +1,10 @@
+import { describe, expect, it } from 'vitest'
 import { Item, Shop } from "./gilded-rose";
 
 describe("Gilded Rose", () => {
   it("should foo", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("foo");
   });
 });
